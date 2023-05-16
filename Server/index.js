d0228f04b6368b042da92a9ce2d591f0d9915e3a
samtessema9 <99148321+samtessema9@users.noreply.github.com>
2023-05-16 12:32:36 -0700

    added database connection logic

--- Server/index.js
+++ Server/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+const connectToDB = require('./src/config/db')
+
+// Connect to the database
+connectToDB()
+
 // Handle all requests
 app.use('*', (req, res) => {
     console.log(`request received from ip address - ${req.ip}`);
     res.json({response: 'response'});
 });
 
+// start the server and listen on port 3000
 app.listen(port, () => console.log('listening on port ' + port));
