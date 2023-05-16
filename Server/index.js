8941a0d9269b35805ce0e02b7a18a523baf9fa24
sam <samtessema09@gmail.com>
2023-05-15 17:21:22 -0700

    gitignored node_modules

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const port = 3000
 // Handle all requests
 app.use('*', (req, res) => {
     console.log(`request received from ip address - ${req.ip}`);
-    res.json({response: 'succesful request'});
+    res.json({response: 'response'});
 });
 
 app.listen(port, () => console.log('listening on port ' + port));
