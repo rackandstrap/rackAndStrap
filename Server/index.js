6c41edc35b8e6fd4639f0fa7b2425b262c36eeb3
sam tessema <99148321+samtessema9@users.noreply.github.com>
2023-06-13 20:57:39 -0700

    completed CRUD functionality for users and jobs routes

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000
+const port = 3001
 
 const connectToDB = require('./src/config/db')
 // Connect to the database
