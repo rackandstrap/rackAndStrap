b2d598aeda839398abdef3b286e331de6190604b
nonwor <nonwor@gmail.com>
2023-06-21 02:02:54 -0700

    done with css

--- client/src/componets/CreateJob/index.js
+++ client/src/componets/CreateJob/index.js
@@ -11,22 +11,93 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 const CreateJob = () => {
+
+    //Set style for the +button 
     const myStyle = {
         alignSelf: 'flex-end'
-      };
+    };
+
+    //Dynamic text for request vs service
+    const request_copy = {
+        "titlePlaceHolder":"My boat need a ride",
+        "equipmentTitle":"Equipment:",
+        "descriptionPlaceHolder":"Need help moving a WAKA, flexible on dates",
+        "from":"From:",
+        "to":"To:"
+    }
+    const service_copy = {
+        "titlePlaceHolder":"Going Gauley Fest, room for 2 boats!",
+        "equipmentTitle":"Space for:",
+        "descriptionPlaceHolder":"Going to Gauley Fest, space for boats, please reach out!",
+        "from":"Leaving:",
+        "to":"Arriving:"
+    }
+    const[copyText, setCopyText] = useState(request_copy);
+
+    //Main jobData to send to the backend
     const[jobData, setJobData] = useState({})
 
-    const[equipmentAndQuanity, setEquipmentAndQuanity] = useState([{"kayk":1}])
+    //Equipment Quanity will be send as an equipment object as part
+    //of the main data
+    const[equipmentAndQuanity, setEquipmentAndQuanity] = useState({});
+    const[selectEquipment, setEquipment] = useState('Creek Boat');
+    const[selectQuantity, setQuantity] = useState(0);
+
+    
+    //We will only show equipment that have quanity more than
+    const removeItemFromEquipmentObject=(key)=>{
+        
+        console.log("removing:", key)
+        setEquipmentAndQuanity(equipmentAndQuanity =>({
+            ...equipmentAndQuanity,
+            [key]:0,
+        }));
+        console.log(equipmentAndQuanity)
+    }
+
+    const handleSelectEquipment = (event) => {
+        setEquipment(event.target.value)
+    }
+    const handleSelectQuantity = (event) => {
+        setQuantity(Number(event.target.value))
+    }
+
+    let equipmentRender = Object.entries(equipmentAndQuanity).map(([key, value]) => {
+        if(value > 0){
+            return(
+                <div key={key} className="equipmentLabel">
+                    <span>{key}: </span>
+                    <span className="valueCount">{value}</span>
+                    <Button  className="btn btn-danger btn-xs" id="delete" onClick={()=>removeItemFromEquipmentObject(key)}>
+                        -
+                    </Button>
+                </div>
+            )
+        } else {
+            return null
+        }
+    })
 
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
-    const [selectedOption, setSelectedOption] = useState('');
+    const [selectedOption, setSelectedOption] = useState('request');
+
     useEffect(() => {
         console.log('Selected option:', selectedOption);
     }, [selectedOption]);
+
+    const handleSelectModeChange =(value)=>{
+        setSelectedOption(value)
+        if(value == 'request'){
+            setCopyText(request_copy)
+        } else {
+            setCopyText(service_copy)
+        }
+    }
 
     const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
@@ -35,7 +106,10 @@ const CreateJob = () => {
     ))
     
     const handleAddButtonClick=()=>{
-
+        setEquipmentAndQuanity(equipmentAndQuanity =>({
+            ...equipmentAndQuanity,
+            [selectEquipment]:selectQuantity,
+        }));
     }
 
     return (
@@ -47,12 +121,12 @@ const CreateJob = () => {
                 <div key={`inline-${type}`} className="mb-3">
                 <Form.Check
                     inline
-                    label="Looking for a ride!"
+                    label={"Looking for a ride!"}
                     name="group1"
                     type={type}
                     id={`inline-${type}-1`}
-                    checked={selectedOption === 'ride'}
-                    onChange={() => setSelectedOption('ride')}
+                    checked={selectedOption === 'request'}
+                    onChange={() => handleSelectModeChange('request')}
                     />
                     <Form.Check
                     inline
@@ -60,8 +134,8 @@ const CreateJob = () => {
                     name="group1"
                     type={type}
                     id={`inline-${type}-2`}
-                    checked={selectedOption === 'rackSpace'}
-                    onChange={() => setSelectedOption('rackSpace')}
+                    checked={selectedOption === 'service'}
+                    onChange={() => handleSelectModeChange('service')}
                     />
                 </div>
                 ))}
@@ -70,7 +144,7 @@ const CreateJob = () => {
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="">
                         <Form.Label>Title: </Form.Label>
-                        <Form.Control type="text" placeholder="Need a ride for my kayak!" />
+                        <Form.Control type="text" placeholder={copyText.titlePlaceHolder} />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridBid">
@@ -78,7 +152,7 @@ const CreateJob = () => {
                         <InputGroup className="mb-3">
                         <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
                         <Form.Control
-                        placeholder="$$ optional $$"
+                        placeholder="optional"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
                         />
@@ -87,27 +161,27 @@ const CreateJob = () => {
                 </Row>
 
                 <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Equipment</Form.Label>
-                    <Form.Select defaultValue="Choose...">
-                        <option>Creek Boat</option>
-                        <option>Play Boat</option>
-                        <option>Half Slice</option>
-                        <option>Full Slice</option>
-                        <option>Canoe</option>
-                        <option>Bike</option>
-                    </Form.Select>
+                    <Form.Group as={Col} controlId="formGridStateEquipment">
+                        <Form.Label>{copyText.equipmentTitle}</Form.Label>
+                        <Form.Select defaultValue="Choose..." onChange={handleSelectEquipment}>
+                            <option>Creek Boat</option>
+                            <option>Half Slice</option>
+                            <option>Play Boat</option>
+                            <option>Full Slice</option>
+                            <option>Canoe</option>
+                            <option>Bike</option>
+                        </Form.Select>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Quantity</Form.Label>
-                    <Form.Select defaultValue="Choose...">
-                        <option>0</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                    </Form.Select>
+                    <Form.Group as={Col} controlId="formGridStateQuantity">
+                        <Form.Label>Quantity</Form.Label>
+                        <Form.Select defaultValue="Choose..." onChange={handleSelectQuantity}>
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </Form.Select>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridState" style={myStyle}>
@@ -120,20 +194,19 @@ const CreateJob = () => {
                 </Row>
 
                 {/* We append equipment type and quanity here*/}
-                {/* {equipmentAndQuanity.map((item, index) => (
-                    <p key={index}>{item}</p>
-                ))} */}
+            
+                {equipmentRender}
 
                 <FloatingLabel controlId="floatingTextarea2" label="Detailed Description">
                 <Form.Control
-                as="textarea"
-                placeholder="Leave a comment here"
-                style={{ height: '100px' }}
-                />
+                    as="textarea"
+                    placeholder="Leave a comment here"
+                    style={{ height: '80px' }}
+                    />
                 </FloatingLabel>
     
         <Row className="mb-3">
-            <span>Departure:</span>
+            <span>{copyText.from}</span>
              
             <Form.Group as={Col} controlId="formGridCity">
             <Form.Label>City</Form.Label>
@@ -159,7 +232,7 @@ const CreateJob = () => {
         </Row>
 
         <Row className="mb-3">
-            <span>Arrival:</span>
+            <span>{copyText.to}</span>
              
             <Form.Group as={Col} controlId="formGridCity">
             <Form.Label>City</Form.Label>
