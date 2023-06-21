ef2d6f23722c8bedfdbc711ee311ecf6df9e1e5f
nonwor <nonwor@gmail.com>
2023-06-20 21:07:09 -0700

    nav bar with bootstrap to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import User from './userComponent/UserProfile';
 import Login from './loginComponent/Login';
 import LandingPage from './pages/LandingPage/LandingPage.js';
+import Navigate from './pages/Nav/index.js';
 
 function App() {
   return (
     <div className="App">
       Rack and Strap
+      <Navigate/>
       <LandingPage />
       <Login/>
       {/* <User/> */}
