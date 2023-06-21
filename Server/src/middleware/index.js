c6d1c9816ec577c741dba05602e51bd2b43568af
sam <samtessema09@gmail.com>
2023-06-20 22:43:17 -0700

    added error handling to auth middleware  and added new jobs and users

--- Server/src/middleware/index.js
+++ Server/src/middleware/index.js
@@ -5,30 +5,26 @@ require('dotenv').config('../config/.env');
 
 const authenticateUser = async (req, res, next) => {
 
-    const token = req.headers.authorization.split(' ')[1];
+    try {
+        const token = req.headers.authorization.split(' ')[1];
 
-    if (!token) {
-        return res.status(403).json({response: "please include a token"})
+        if (!token) {
+            return res.status(403).json({response: "please include a token"})
+        }
+
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (data) {
+            const user = await User.findOne({_id: data.id})
+            req.user = user;
+            next()
+        } else {
+            res.status(403).json({response: "error"})
+        }
+    } 
+    catch (err) {
+        res.status(500).json({response: err});
     }
-
-    const data = jwt.verify(token, process.env.JWT_SECRET)
-
-    if (data) {
-        const user = await User.findOne({_id: data.id})
-        req.user = user;
-        next()
-    } else {
-        res.status(403).json({response: "error"})
-    }
-
-    // const user = await User.findOne({_id: id})
-
-    // if (!user) {
-    //     return res.status(403).json({response: "wrong credentials"})
-    // }
-
-    // req.user = user
-
 }
 
 module.exports = {authenticateUser}
