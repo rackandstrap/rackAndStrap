31abdc75699c15ddd54ddf6b5cae8142ca84d586
nonwor <nonwor@gmail.com>
2023-06-29 09:39:09 -0700

    update code styling

--- client/src/pages/Nav/index.js
+++ client/src/pages/Nav/index.js
@@ -34,69 +34,64 @@ const NavigateBar = () => {
     if(login_status){
         return(
             <>
-            <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand as={Link} to="/home">Rack & Strap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    {/* <Nav.Link href="#home">Need</Nav.Link>
-                    <Nav.Link href="#link">Got Room!</Nav.Link> */}
-                </Nav>
-                
-                <Nav className='logged-in-view'>
-                    <Nav.Link href="#createpost">Create Post!</Nav.Link>
-                    <NavDropdown title="Profile" id="basic-nav-dropdown" className="ml-auto">
-                        <NavDropdown.Item as={Link} to='/userprofile'> Profile </NavDropdown.Item>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/home">Rack & Strap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {/* <Nav.Link href="#home">Need</Nav.Link>
+                            <Nav.Link href="#link">Got Room!</Nav.Link> */}
+                        </Nav>
                         
-                        <NavDropdown.Item href="#action/3.2">
-                            My Posts
-                        </NavDropdown.Item>
-                        
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item onClick={handleLogoutAction}>
-                            Log Out
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-    
-        </>       
+                        <Nav className='logged-in-view'>
+                            <Nav.Link href="#createpost">Create Post!</Nav.Link>
+                            <NavDropdown title="Profile" id="basic-nav-dropdown" className="ml-auto">
+                                <NavDropdown.Item as={Link} to='/userprofile'> Profile </NavDropdown.Item>
+                                
+                                <NavDropdown.Item href="#action/3.2">
+                                    My Posts
+                                </NavDropdown.Item>
+                                
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={handleLogoutAction}>
+                                    Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>       
         );
     }else{
 
         /* Logged out state here*/ 
         return(
             <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand as={Link} to="/landingPage">Rack & Strap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/landingPage">Rack & Strap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
 
-                    <Nav className="me-auto">
-                        {/* <Nav.Link href="#home">Need</Nav.Link>
-                        <Nav.Link href="#link">Got Room!</Nav.Link> */}
-                    </Nav>
-                    {/* For loggin/signup */}
-                    <Nav className='logged-out-view'>
-                        <div className='custom-link'>
-                            <Link to="/login">Login/Sign Up</Link>
-                        </div>
-                    </Nav>
-                    
-                </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            
-        
-            </>
-            
+                        <Nav className="me-auto">
+                            {/* <Nav.Link href="#home">Need</Nav.Link>
+                            <Nav.Link href="#link">Got Room!</Nav.Link> */}
+                        </Nav>
+                        {/* For loggin/signup */}
+                        <Nav className='logged-out-view'>
+                            <div className='custom-link'>
+                                <Link to="/login">Login/Sign Up</Link>
+                            </div>
+                        </Nav>
+                        
+                    </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </> 
         );
     }
-    
 }
 
 export default NavigateBar;
